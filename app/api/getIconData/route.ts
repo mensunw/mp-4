2d16569c26b7fcc48ca086c1af48bbe653c0d9b3
Mensun <mensun@bu.edu>
2025-03-29 17:57:45 -0400

    installed shadcn, configured API and successfully got it to work with desired search and functioanility

---
+++ app/api/getIconData/route.ts
@@ -0,0 +1,35 @@
+"use server"
+import { NextResponse, NextRequest } from 'next/server';
+
+
+// here's where i got info on this: https://nextjs.org/blog/building-apis-with-nextjs#3-creating-an-api-with-route-handlers
+
+export async function GET(request: NextRequest) {
+  try {
+    // get query data
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('query');
+    console.log(query)
+    const response = await fetch(`https://api.iconfinder.com/v4/icons/search?query=${query}&count=10`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.API_KEY}`,
+      },
+    });
+
+    // parse response
+    const data = await response.json();
+
+    // additional error checking might be needed depending on API here
+    // return data along with success status
+    return NextResponse.json({ message: data }, { status: 200 });
+
+  } catch (e) {
+    console.error('Hello API Error:', e);
+    return NextResponse.json(
+      { error: 'Failed to fetch data from the backend (Internal Server Error)' },
+      { status: 500 }
+    );
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
+  const [query, setQuery] = useState('');
+  const [icons, setIcons] = useState([]);
+
+  // get icons using api call
+  async function getIcons() {
+    // insert query into api call
+    const response = await (await fetch(`/api/getIconData?query=${encodeURIComponent(query)}`)).json()
+    console.log(response)
+  }
+
   return (
     <>
-      hi
+      <div className="flex flex-row">
+        <Input onChange={(e) => setQuery(e.target.value)} placeholder="Enter desired icon e.g Github, Linkedin"></Input>
+        <Button variant="default" onClick={() => (getIcons())}>Search</Button>
+      </div>
     </>
   );
 }
